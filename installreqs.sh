7e0e25c15ce079f38b224bfd09c18e3358a95db6
AranNomante <altug.ceylan.YES@gmail.com>
2020-07-26 05:54:47 +0300

    update

--- installreqs.sh
+++ installreqs.sh
@@ -3,25 +3,25 @@ before_reboot(){
     # Do stuff
 	sudo add-apt-repository ppa:oibaf/graphics-drivers
 	sudo apt update
-	sudo apt dist-upgrade
+	sudo apt -y dist-upgrade
 }
 
 after_reboot(){
     # Do stuff
-	sudo apt install va-driver-all
-	sudo apt install vainfo
+	sudo apt -y install va-driver-all
+	sudo apt -y install vainfo
 	sudo vainfo
-	sudo apt install ffmpeg
-	sudo apt install chromium
+	sudo apt -y install ffmpeg
+	sudo apt -y install chromium
 }
 
 if [ -f /var/run/rebooting-for-updates ]; then
     after_reboot
     rm /var/run/rebooting-for-updates
-    update-rc.d installreqs.sh remove
+    sudo update-rc.d installreqs.sh remove
 else
     before_reboot
-    touch /var/run/rebooting-for-updates
-    update-rc.d installreqs.sh defaults
+    sudo touch /var/run/rebooting-for-updates
+    sudo update-rc.d installreqs.sh defaults
     sudo reboot
 fi
