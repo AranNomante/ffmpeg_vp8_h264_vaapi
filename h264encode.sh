3cc1ad08c403246786e1b34403a23f7dabd08c9b
AranNomante <altug.ceylan.YES@gmail.com>
2020-07-27 07:35:13 -0400

    h264

--- h264encode.sh
+++ h264encode.sh
@@ -2,12 +2,13 @@
 #$2 no threads to use
 #$3 preset ultrafast superfast veryfast faster fast medium slow slower veryslow
 #$4 min bitrate specify around 1k when using slower encoding else put 0-500
-#$5 out filename
+#$5 crf 0 means lossless don't go over 30 - 22-25 is a good range
+#$6 out filename
 # -b:v bitrate 512k maybe too low for 720p30 try 1500k to 2000k 
 #for fast/quality use realtime 6-7 
 #for very good quality use good 2
 start=$(date +%s.%N)
-ffmpeg -i $1 -r 30 -g 90 -s 1280x720 -aspect 16:9 -c:v libx264 -preset $3  -b:v 2500k -threads $2 -minrate $4  -maxrate 3000k -crf 30  $5.mp4
+ffmpeg -i $1 -r 30 -g 90 -s 1280x720 -aspect 16:9 -c:v libx264 -preset $3  -b:v 2500k -threads $2 -minrate $4  -maxrate 3000k -bufsize 3000k -crf $5  $6.mp4
 
 duration=$(echo "$(date +%s.%N) - $start" | bc)
 execution_time=`printf "%.2f seconds" $duration`
